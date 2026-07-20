900d10e5e4eeb14d53ab54ee06490b4ef8fdb52b
sofiisunreal <sophiekendi17@gmail.com>
2026-07-20 11:58:31 +0300

    wsgi

--- maziwasyncbck/wsgi.py
+++ maziwasyncbck/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 import sys 
-sys.path.append("home/sophieemp/www/maziwasyncbck/maziwasyncbck/wsgi.py")
+sys.path.append("/home/sophieemp/www/maziwasyncbck/")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maziwasyncbck.settings')
