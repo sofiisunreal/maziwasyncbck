e4f3e9e87449051dad24cb09a1f4e2443ade4a36
sofiisunreal <sophiekendi17@gmail.com>
2026-07-23 11:50:56 +0300

    cors allowed

--- maziwasyncbck/settings.py
+++ maziwasyncbck/settings.py
@@ -32,6 +32,11 @@ ALLOWED_HOSTS = [
      'sophieemp.alwaysdata.net'
 ]
 
+CORS_ALLOWED_ORIGINS=[
+    "http://localhost:5173",
+    "http://127.0.0.1:8000"
+]
+
 AUTH_USER_MODEL='core.User'
 
 # Application definition
